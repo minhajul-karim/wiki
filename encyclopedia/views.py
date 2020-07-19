9bea0156e8d4b8019dcadf911448ab90f967cbb3
minhajul-karim <minhajul.kaarim@gmail.com>
2020-07-19 13:00:59 +0600

    Display entry or error page based on title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import markdown2
+
 from . import util
 
 
 def index(request):
+    """List of all available wiki entries."""
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def display_entry(request, title):
+    """Display wiki entry."""
+    markdown_content = util.get_entry(title)
+    if markdown_content:
+        return render(request, "encyclopedia/entry.html", {
+            "content": markdown2.markdown(markdown_content),
+            "title": title})
+    return render(request, "encyclopedia/error.html")
